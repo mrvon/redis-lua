7694fc1f680a157c538ab999f3693429d8f5809f
dennis <mrvon@qq.com>
2015-11-19 15:09:50 +0800

    merge

--- src/merge_server_data_handler.lua
+++ src/merge_server_data_handler.lua
@@ -2,9 +2,11 @@ require "DB_KEY"
 
 local handler = {}
 
-handler[KEY_DIAMOND_INVEST_GLOBAL] = function(data_table)
-    data_table.local_open_id = data_table.local_open_id + 1
-    return data_table
+handler[KEY_DIAMOND_INVEST_GLOBAL] = function(a_table, b_table)
+    -- print(a_table and "a local_open_id: " .. a_table.local_open_id)
+    -- print(b_table and "b local_open_id: " .. b_table.local_open_id)
+    local n_table = {}
+    return n_table
 end
 
 return handler
